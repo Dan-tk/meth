46766998e7771dbdfa6899f8a4930e703dc1a495
Dan-tk <168034436+Dan-tk@users.noreply.github.com>
2024-07-15 18:32:55 +0300

    Update allowedOrigins.ts

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     `http://localhost:5173`,
-    `https://finale-frontend-beta.onrender.com`
+    `https://finale-frontend-beta.onrender.com`,
+    "https://froo.onrender.com"
 ]
 
 export default allowedOrigins
